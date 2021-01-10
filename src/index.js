cf3a65fcda4926f5c31e545ce698560ae46b9d85
Samuel Martinez <samuelmtz2000@hotmail.com>
2021-01-10 13:35:07 -0600

    commit on 3_bookProject(listas-simples)

--- src/index.js
+++ src/index.js
@@ -5,39 +5,43 @@ import ReactDom from 'react-dom'
 import './index.css';
 
 //setup vars
-const Libro1 ={
+const Libros =[{
   img: "https://m.media-amazon.com/images/I/91zzQmzB2-L._AC_UL320_.jpg",
   title: 'Sherlock Holmes',
   author: 'Sir Arthur Conan Doyle'
-}
-const Libro2 ={
+},
+ {
   img: "https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._SX329_BO1,204,203,200_.jpg",
   title: 'Atomic Habits',
   author: 'James Clear'
-}
+}];
+//En react puedes renderizar arrays de strings solamente, con objetos te va a arrojar un error
+//Ejemplo aqui renderiza la lista de nombres que mapeamos para que sean objetos (nombresH1) <h1>
 
+const nombres = [ 'juan','jose','jesus'];
+const nombresH1 = nombres.map((nombre)=> {
+return <h1>  {nombre}  </h1>;
+});
+
+console.log(nombresH1);
 function BookList(){
 
   return  (
     <section className="booklist">
-      <Book img={Libro1.img} title={Libro1.title} author={Libro1.author}>
-        {/* Children props se agregan dentro del contenedor */}
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, consequatur quasi laboriosam dolorem sequi tempora enim explicabo repellendus. Voluptatum, sequi.</p>
-      </Book>
-      <Book img={Libro2.img} title={Libro2.title} author={Libro2.author}/>
-
+      {/* Se muestra simplemente agregando el array de objetos (h1) como return */}
+      {nombresH1}
     </section>
-  );
+  )
 }
 //La variable childres se pasa como un objeto de prop por eso la decotrur como las otras propiedades
-const Book = ({img, title, author, children}) => {
+const Book = ({img, title, author}) => {
   //const {img, title, author} = props;
   return (
     <article className="book"> 
-      <img src={img}alt="img-slk"/>
+      <img src={img} alt="img-slk"/>
       <h1>{title}</h1>
       <h5 className="author">{author.toUpperCase()}</h5>
-      {children}
+      
     </article>
   );
 }
